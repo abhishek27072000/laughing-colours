553f1d0781f01f120f7aec8c41036018d71e530f
Your Name <you@example.com>
2020-04-25 10:55:11 +0530

    add file

---
+++ count.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,fact=0,n;
+    cout<<"enter n :";
+    cin>>n;
+    cout<<endl;
+    for(i=1;i<=n;i++)
+    {
+        fact=fact+i;
+    }
+    cout<<fact;
+}
